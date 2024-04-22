c5c411b11fe1662898e3fe9cfcdb0da7605326c7
Abbas Anosh <abbasanosh123@gmail.com>
2024-04-22 10:30:51 -0400

    homepage changes

--- frontend/src/pages/homePage/HomePage.jsx
+++ frontend/src/pages/homePage/HomePage.jsx
@@ -3,17 +3,30 @@ import "./homepage.css";
 /*import "./playgroundpal_logo_color.svg"; */
 
 const HomePage = () => {
-  return <div className="home">
-    <img src="playgroundpal_logo_color.svg" alt="color logo" className="color_logo_landing" />
-    <h2>Welcome To</h2>
-    <h1>Playground Pal</h1>
-    <p>Welcome to our Playpal app, an easy way to find playgrounds near you! To begin please click on the button below to access our main page where you will be able to find any playgrounds with specific fratures.</p>
-    <button type="button" className="landing_page_button" >Let's Get Started</button>
-    <img src="PlaygroundPal_hand_icon.svg" alt="hand icon" className="hand_icon_landing" />
-    
-  </div>;
+  return (
+    <div className="home">
+      <img
+        src="playgroundpal_logo_color.svg"
+        alt="color logo"
+        className="color_logo_landing"
+      />
+      <h2>Welcome To</h2>
+      <h1>Playground Pal</h1>
+      <p>
+        Welcome to our Playpal app, an easy way to find playgrounds near you! To
+        begin please click on the button below to access our main page where you
+        will be able to find any playgrounds with specific fratures.
+      </p>
+      <button type="button" className="landing_page_button">
+        Let's Get Started
+      </button>
+      <img
+        src="PlaygroundPal_hand_icon.svg"
+        alt="hand icon"
+        className="hand_icon_landing"
+      />
+    </div>
+  );
 };
 
-
 export default HomePage;
-/*<img src={playgroundPalLogo} alt="Playground Pal Logo"/>*/
--- frontend/src/pages/layout/Layout.jsx
+++ frontend/src/pages/layout/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import "./Layout.css";
-import Footer from "../../components/footer/Footer"
+import Footer from "../../components/footer/Footer";
 const Layout = () => {
   return (
     <div className="layout">
@@ -12,7 +12,7 @@ const Layout = () => {
       <div className="content">
         <Outlet />
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };

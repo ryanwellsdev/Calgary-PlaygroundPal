08c1544022de482b83ce905232bcceea818a75ea
Abbas Anosh <abbasanosh123@gmail.com>
2024-05-05 16:36:46 -0600

    env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": process.env.REACT_APP_BACKEND_URL,
     },
   },
 });

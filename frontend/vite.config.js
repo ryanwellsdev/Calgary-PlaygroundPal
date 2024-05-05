db7ee101b4e3bb93ef2e3c36f364700e83c8ad55
Abbas Anosh <abbasanosh123@gmail.com>
2024-05-05 16:50:21 -0600

    new configuration for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": process.env.REACT_APP_BACKEND_URL,
+      "/api": "https://inceptionu-calgary-playgroundpal.onrender.com",
     },
   },
 });

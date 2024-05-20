15cebf5a3bed7bd78d646465fc396d0c7a84c103
Abbas Anosh <abbasanosh123@gmail.com>
2024-05-20 13:01:13 -0600

    sliced

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
         "https://inceptionu-calgary-playgroundpal.onrender.com/api/playground"
       );
       const data = await response.json();
-      const limitedData = data;
+      const limitedData = data.slice(0, 100);
       setPlaygroundInfo(limitedData);
       console.log(data, "dfafsdfsdf");
     } catch (error) {

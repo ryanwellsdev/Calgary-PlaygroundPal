b6ee6b6d70e755cd33b9496ee0e794446011ff42
ryanwellsdev <ryanwellsdev@gmail.com>
2025-05-10 15:33:32 +0100

    back to working

--- backend/routes/PlaygroundRoute.js
+++ backend/routes/PlaygroundRoute.js
@@ -1,18 +1,46 @@
+import express from "express";
+const router = express.Router();
+
+import PlaygroundSurface from "../models/PlaygroundSurface.js";
+import PlaygroundEquipment from "../models/PlaygroundEquipments.js";
+import PlaygroundName from "../models/PlaygroundName.js";
+
+router.get("/:assetCd", async (req, res) => {
+  try {
+    const { assetCd } = req.params;
+    const surfaceData = await PlaygroundSurface.findOne({ ASSET_CD: assetCd });
+    const equipmentData = await PlaygroundEquipment.findOne({
+      ASSET_CD: assetCd,
+    });
+    const nameData = await PlaygroundName.findOne({ ASSET_CD: assetCd });
+
+    const playgroundData = {
+      surface: surfaceData,
+      equipment: equipmentData,
+      name: nameData,
+    };
+
+    res.json(playgroundData);
+  } catch (error) {
+    console.error("Error fetching playground data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 router.get("/", async (req, res) => {
   try {
-    const equipmentData = await PlaygroundEquipment.find({}).lean();
-    const nameData = await PlaygroundName.find({}).lean();
-    const surfaceData = await PlaygroundSurface.find({}).lean();
+    const equipmentData = await PlaygroundEquipment.find({});
 
-    if (!equipmentData.length) {
-      return res.status(404).json({ error: "No equipment data found" });
-    }
+    const nameData = await PlaygroundName.find({});
+
+    const surfaceData = await PlaygroundSurface.find({});
 
     const allPlaygrounds = equipmentData.map((equipment) => {
       const assetCd = equipment.ASSET_CD;
-
-      const name = nameData.find((n) => n.ASSET_CD === assetCd) || {};
-      const surface = surfaceData.find((s) => s.ASSET_CD === assetCd) || {};
+      const name = nameData.find((name) => name.ASSET_CD === assetCd);
+      const surface = surfaceData.find(
+        (surface) => surface.ASSET_CD === assetCd
+      );
 
       return {
         ASSET_CD: assetCd,
